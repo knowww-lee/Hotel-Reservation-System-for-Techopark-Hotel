8e5109d4a74e49935435d992aea632b5c8122037
roshanjill <roshanjillrvillaluna@iskolarngbayan.pup.edu.ph>
2025-01-17 01:01:19 +0800

    Update Sidebar.jsx

--- resources/js/Layouts/Sidebar.jsx
+++ resources/js/Layouts/Sidebar.jsx
@@ -5,17 +5,12 @@ export default function Sidebar() {
     return (
 <aside class="w-80 bg-white shadow-lg">
   <div class="p-6 text-center">
-      <div className="flex shrink-0 items-center">
-                                <Link href="/">
-                                   
-                                        <div className="flex items-center w-3/4"> 
-                                        <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                            <h2 className="text-white text-2xl">
-                                                <span className="font-bold">TECHNOPARK</span><br />
-                                                HOTEL
-                                            </h2>
-                                        </div>
-                                </Link>
+      <div className="flex justify-center items-center">
+            <Link href="/">
+                <div className="flex items-center"> 
+                    <ApplicationLogo />
+                </div>
+            </Link>
         </div>
     <h2 class="text-2xl font-bold text-gray-800">Technopark Hotel</h2>
   </div>
