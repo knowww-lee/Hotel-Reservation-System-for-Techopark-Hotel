1f0108deb8a49ccdb3846fe22c83a50a36696b95
Zertz24 <eugeniopablo71@gmail.com>
2025-01-17 00:06:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/room', function () {
+    return Inertia::render('Room');
+})->name('room');
+
 Route::get('/frontdesk', [FrontDeskController::class, 'index'])->name('frontdesk');
 Route::get('/guest', [GuestController::class, 'index'])->name('guest');
 Route::get('/rooms', [RoomsController::class, 'index'])->name('rooms');
